57884c14789adf93f4ce9863ca148cb8a6cec045
liujb_a <liujb_a@si-tech.com.cn>
2022-07-17 14:46:43 +0800

    liujunbao

--- src/main/java/com/atliujunbao/GitTest.java
+++ src/main/java/com/atliujunbao/GitTest.java
@@ -2,16 +2,6 @@ package com.atliujunbao;
 
 public class GitTest {
     public static void main(String[] args){
-        System.out.println("hello git");
-        System.out.println("hello git2");
-        System.out.println("hello git3");
-        System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot_fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
-        System.out.println("gitee test");
 
-        System.out.println("hot_fix git push test");
     }
 }
